fee13c58760d441c254009ab2c8bcd5077a2e329
Arthur Lovera <arthuralbaralovera@gmail.com>
2025-09-20 18:56:51 -0300

    ex14

---
+++ 14.py
@@ -0,0 +1,31 @@
+#verifica se a nota é valida (max 10)
+while True:
+    nota1 = float(input('Primeira nota: '))
+    if nota1 > 10:
+        print('-'*30)
+        print('\033[31mERRO! Nota max: 10\033[m')
+    else:
+        break
+print('-'*30)
+while True:
+    nota2 = float(input('Segunda nota: '))
+    if nota2 > 10:
+        print('-'*30)
+        print('\033[31mERRO! Nota max: 10\033[m')
+    else:
+        break
+
+media = (nota1 + nota2) / 2
+
+#verifica o aproveitamento
+if media < 4:
+    print('Seu aproveitamento foi \033[31;1mE\033[m. Melhore1')
+elif media >= 4 and media < 6:
+    print('Seu aproveitamento foi \033[31mD\033[m. Melhore!')
+elif media >= 6 and media < 7.5:
+    print('Seu aproveitamento foi \033[33;1mC\033[m. Não foi tão mal!')
+elif media >= 7.5 and media < 9:
+    print('Seu aproveitamento foi \033[34;1mB\033[m. Uau, foi bem!')
+else:
+    print('Seu aproveitamento foi \033[32;1mA\033[m. Você está de Parabéns!')
+print('-'*30)
