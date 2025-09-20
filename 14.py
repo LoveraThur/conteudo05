37f877bcae502f097cc55b288dbe0e55320af4a3
Arthur Lovera <arthuralbaralovera@gmail.com>
2025-09-20 19:02:39 -0300

    ex14

--- 14.py
+++ 14.py
@@ -14,18 +14,24 @@ while True:
         print('\033[31mERRO! Nota max: 10\033[m')
     else:
         break
+print('-'*30)
 
 media = (nota1 + nota2) / 2
 
 #verifica o aproveitamento
-if media < 4:
-    print('Seu aproveitamento foi \033[31;1mE\033[m. Melhore1')
-elif media >= 4 and media < 6:
-    print('Seu aproveitamento foi \033[31mD\033[m. Melhore!')
-elif media >= 6 and media < 7.5:
-    print('Seu aproveitamento foi \033[33;1mC\033[m. Não foi tão mal!')
-elif media >= 7.5 and media < 9:
-    print('Seu aproveitamento foi \033[34;1mB\033[m. Uau, foi bem!')
+print(f'Suas notas foram: {nota1} e {nota2}.\nSua média foi {media}')
+if media < 6:
+    if media < 4:
+        print('Seu aproveitamento foi \033[31;1mE\033[m. Melhore!')
+    else:
+        print('Seu aproveitamento foi \033[31mD\033[m. Melhore!')
+    print('\033[31mREPROVADO\033[m')
 else:
-    print('Seu aproveitamento foi \033[32;1mA\033[m. Você está de Parabéns!')
+    if media >= 6 and media < 7.5:
+        print('Seu aproveitamento foi \033[33;1mC\033[m. Não foi tão mal!')
+    elif media >= 7.5 and media < 9:
+        print('Seu aproveitamento foi \033[34;1mB\033[m. Uau, foi bem!')
+    else:
+        print('Seu aproveitamento foi \033[32;1mA\033[m. Você está de Parabéns!')
+    print('\033[32mAPROVADO\033[m')
 print('-'*30)
